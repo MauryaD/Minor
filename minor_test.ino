a140b34f614b2900e1a819cf9c630ef14b4f1508
DEEPAK MAURYA <deepakmaurya2198@gmail.com>
2019-11-17 14:29:58 +0530

    Update minor_test.ino
    Almost the final code. 2-3 minor changes needed.!

--- minor_test.ino
+++ minor_test.ino
@@ -1,20 +1,27 @@
+//int left, right, forward, backward;
+const int motorPin1  = 5;  
+const int motorPin2  = 6;  
+//Motor B
+const int motorPin3  = 10; 
+const int motorPin4  = 9;  
 void setup() {
+  Serial.begin(9600);
   // pin 0 and 1 == Rx and Tx resp, are connected with Tx and Rx of Bolt.
   pinMode(2,INPUT); //pin connected to BOLT   frwd -> P1
   pinMode(4,INPUT); //pin connected to BOLT   bkwd -> P2
   pinMode(7,INPUT); //pin connected to BOLT   lft -> P3
   pinMode(8,INPUT); //pin connected to BOLT   ryt -> P4
-
-  pinMode(5,OUTPUT); //for motors
-  pinMode(6,OUTPUT); //5-6 M1
-  pinMode(10,OUTPUT);  //9-10 M2
-  pinMode(9,OUTPUT); //for motors
+  pinMode(3,INPUT); // to stop Mickey!
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
 
   pinMode(12, OUTPUT);  //for dist
   pinMode(11, INPUT);
 }
 
-void calcdist()
+float calcdist()
 {
   float duration, distance;
   digitalWrite(12, LOW);
@@ -27,53 +34,92 @@ void calcdist()
   //Serial.print("Distance: ");
   //Serial.println(distance);
   //delay(100);  
-  if(distance < 20)
-    {
-      digitalWrite(5,LOW);
-      digitalWrite(6,LOW);
-      digitalWrite(10,LOW);
-      digitalWrite(9,LOW);
-    }
+//  if(distance < 20)
+//    {
+//      digitalWrite(5,LOW);
+//      digitalWrite(6,LOW);
+//      digitalWrite(10,LOW);
+//      digitalWrite(9,LOW);
+//    }
+return distance;
 }
 
 void loop() {
- // calcdist();
-  int left, right, forward, backward;
+  int left, right, forward, backward,Stop;
   forward = digitalRead(2); //p1
   backward = digitalRead(4);  //p2
   left = digitalRead(7);  //p3
   right = digitalRead(8);  //p4
+  Stop = digitalRead(3);  //p0
+  Serial.print("\t forward:");
+  Serial.print(forward);
+  Serial.print("\t backward:");
+  Serial.print(backward);
+  Serial.print("\t left:");
+  Serial.print(left);
+  Serial.print("\t right:");
+  Serial.print(right);
+ //Serial.print("hello");
   
   if(forward == 1)   //p1
   {
-    digitalWrite(5,HIGH);
-    digitalWrite(6,LOW);
-    digitalWrite(9,HIGH);
-    digitalWrite(10,LOW);
+    backward = digitalRead(4);  
+    left = digitalRead(7);  
+    right = digitalRead(8);
+    Stop = digitalRead(3);
+    digitalWrite(motorPin1, HIGH); //5
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, HIGH);  //10
+    digitalWrite(motorPin4, LOW);
   }
   
   if(backward == 1)  //p2
   {
-    digitalWrite(5,LOW);
-    digitalWrite(6,HIGH);
-    digitalWrite(9,LOW);
-    digitalWrite(10,HIGH);
+    forward = digitalRead(2); 
+    left = digitalRead(7);  
+    right = digitalRead(8);
+    Stop = digitalRead(3);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, HIGH);
   }
-  if( left == 1)  //p3
-  {
-    digitalWrite(5,HIGH);
-    digitalWrite(6,LOW);
-    digitalWrite(9,LOW);
-    digitalWrite(10,HIGH);
+  if(left == 1)  //p3
+  { 
+    backward = digitalRead(4); 
+    forward = digitalRead(2); 
+    right = digitalRead(8);
+    Stop = digitalRead(3);
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
   }
   if(right == 1)   //p4
-  {
-    digitalWrite(5,HIGH);
-    digitalWrite(6,LOW);
-    digitalWrite(9,LOW);
-    digitalWrite(10,HIGH);
+  {   
+    backward = digitalRead(4); 
+    forward = digitalRead(2); 
+    left = digitalRead(7);
+    Stop = digitalRead(3);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
   }
 
-
+  /*if(Stop == 1)
+  {
+    forward = digitalRead(2); 
+    backward = digitalRead(4);
+    left = digitalRead(7);  
+    right = digitalRead(8); 
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    
+  }
+*/
+ 
   
 }
